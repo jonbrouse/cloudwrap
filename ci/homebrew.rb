cc503e4c8b242995977a85f46a6b9bdecf93d9e4
Stephen Cirner <steve@blackfynn.com>
2018-05-29 15:56:16 -0400

    Fix url for tar of packate.

--- ci/homebrew.rb
+++ ci/homebrew.rb
@@ -9,7 +9,7 @@ class Homebrew < Formula
   _ver = '0.2.2'
   version _ver
 
-  url "s3://data.blackfynn.io/public-downloads/#{_repo_name}/#{_bin_name}_#{_ver}_x86_64-apple-darwin.tar.gz"
+  url "https://data.blackfynn.io.s3.amazonaws.com/public-downloads/#{_repo_name}/#{_bin_name}_#{_ver}_x86_64-apple-darwin.tar.gz"
   sha256 '07f31d0479e5d541f5aa44cc5205a3e5373f429ae58a6668109a9e4aabb0f333'
 
   def install
